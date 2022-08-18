5ddbe8d3a01a35572b0f4ca7d22c961c70023048
janetpacheco <anne.janeta@gmail.com>
2022-08-18 13:54:27 -0600

    hello world

---
+++ typescript-training/01-helloworld/helloworld.js
@@ -0,0 +1,5 @@
+console.log('Hello world');
+console.log('Happy birthday Annie' + ' atte Adelita!');
+var firstName = 'Ana';
+var age = 29;
+console.log('Hi ' + firstName + ' today is your birthday ' + age);
---
+++ typescript-training/01-helloworld/helloworld.ts
@@ -0,0 +1,6 @@
+console.log('Hello world');
+console.log('Happy birthday Annie'+' atte Adelita!');
+let firstName : string = 'Ana';
+let age : number = 29;
+console.log('Hi '+ firstName + ' today is your birthday ' + age);
+
