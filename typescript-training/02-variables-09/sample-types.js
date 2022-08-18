3ae3ab8247c2e274b4814d87c54ec87fe89c9852
janetpacheco <anne.janeta@gmail.com>
2022-08-18 13:58:29 -0600

    define variables

---
+++ typescript-training/02-variables-09/sample-types.js
@@ -0,0 +1,13 @@
+var found = true;
+var grade = 98.3;
+var firstName = 'Adelita';
+var lastName = 'Katruska';
+//let's break it
+/*found = 0;
+grade = 'hi';
+firstName = 1;
+*/
+console.log(found);
+console.log('Hi ' + firstName + ' ' + lastName);
+//use template strings 
+console.log("Hi ".concat(firstName));
---
+++ typescript-training/02-variables-09/sample-types.ts
@@ -0,0 +1,16 @@
+let found : boolean = true;
+let grade : number = 98.3;
+let firstName : string = 'Adelita';
+let lastName : string = 'Katruska';
+
+//let's break it
+/*found = 0;
+grade = 'hi';
+firstName = 1;
+*/
+
+console.log(found);
+console.log('Hi ' + firstName + ' ' + lastName);
+
+//use template strings 
+console.log(`Hi ${firstName}`);
