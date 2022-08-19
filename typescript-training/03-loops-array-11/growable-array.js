3def2f036702950bdc09e10ae079be05085b23db
janetpacheco <anne.janeta@gmail.com>
2022-08-19 15:04:15 -0600

    growable arrays push

---
+++ typescript-training/03-loops-array-11/growable-array.js
@@ -0,0 +1,7 @@
+var sports = ['golf', 'foootball', 'tennis'];
+sports.push('badminton');
+sports.push('basketball');
+for (var _i = 0, sports_1 = sports; _i < sports_1.length; _i++) {
+    var tempSport = sports_1[_i];
+    console.log(tempSport);
+}
---
+++ typescript-training/03-loops-array-11/growable-array.ts
@@ -0,0 +1,7 @@
+let sports: string[] = ['golf', 'foootball', 'tennis'];
+sports.push('badminton');
+sports.push('basketball');
+
+for (let tempSport of sports){
+    console.log(tempSport);
+}
