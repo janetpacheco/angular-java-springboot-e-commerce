8217dd0da384ad96dd1c980f0f90cb9f84927ca8
janetpacheco <anne.janeta@gmail.com>
2022-08-22 14:09:13 -0600

    add access modifiers

---
+++ typescript-training/04-creating-classes-14/03-tscconfig-17/Customer.js
@@ -0,0 +1,31 @@
+"use strict";
+var Customer = /** @class */ (function () {
+    function Customer(firstName, lastName) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+    }
+    Object.defineProperty(Customer.prototype, "firstName", {
+        get: function () {
+            return this._firstName;
+        },
+        set: function (value) {
+            this._firstName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Customer.prototype, "lastName", {
+        get: function () {
+            return this._lastName;
+        },
+        set: function (value) {
+            this._lastName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Customer;
+}());
+var customer = new Customer('Annie', 'Pacheco');
+console.log(customer.firstName);
+console.log(customer.lastName);
---
+++ typescript-training/04-creating-classes-14/03-tscconfig-17/Customer.ts
@@ -0,0 +1,36 @@
+class Customer{
+    private _firstName: string;
+    private _lastName: string; 
+    
+    
+    constructor(firstName : string, lastName : string){
+        this._firstName = firstName;
+        this._lastName = lastName;
+    }
+
+
+
+    public get firstName(): string{
+        return this._firstName;
+    }
+
+    public set firstName(value: string){
+        this._firstName = value;
+    }
+
+
+    public get lastName(): string {
+        return this._lastName;
+    }
+    public set lastName(value: string) {
+        this._lastName = value;
+    }
+
+    
+}
+
+let customer = new Customer('Annie', 'Pacheco');
+
+
+console.log(customer.firstName);
+console.log(customer.lastName);
