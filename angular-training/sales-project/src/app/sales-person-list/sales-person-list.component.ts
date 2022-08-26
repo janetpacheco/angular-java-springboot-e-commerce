079c5e6a71b473afacf683b6942bb67bc949457b
janetpacheco <anne.janeta@gmail.com>
2022-08-26 13:14:36 -0600

    highlight table row ngClass

--- angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -13,7 +13,7 @@ export class SalesPersonListComponent implements OnInit {
     new SalesPerson("Ana","Pacheco","ana.pacheco@gmail.com",500000),
     new SalesPerson("Diana","Jimenez","diana.jimenez@gmail.com",300000),
     new SalesPerson("Jose","Camilo","jose_camilo@gmail.com",120000),
-    new SalesPerson("Curtis","Coupal","curtis_coupal@gmail.com",60000)
+    new SalesPerson("Curtis","Coupal","curtis_coupal@gmail.com",5000)
   ];
   constructor() { }
 
