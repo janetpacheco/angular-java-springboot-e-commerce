3ac7ed7f89ccef63f2edf5d35b17f04f49c1ca96
janetpacheco <99910759+AnaPachecoMind@users.noreply.github.com>
2022-08-25 20:22:55 -0600

    create an angular component 34_38

---
+++ angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  // array of of SalesPerson objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Ana","Pacheco","ana.pacheco@gmail.com",500000),
+    new SalesPerson("Diana","Jimenez","diana.jimenez@gmail.com",300000),
+    new SalesPerson("Jose","Camilo","jose_camilo@gmail.com",120000),
+    new SalesPerson("Curtis","Coupal","curtis_coupal@gmail.com",60000)
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
