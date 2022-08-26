fbe99431fd2ab13bec79088216ba5831b54870d2
janetpacheco <anne.janeta@gmail.com>
2022-08-26 17:37:11 -0600

    configure for read only

---
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/huskycode/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,29 @@
+package com.huskycode.ecommerce.config;
+
+import com.huskycode.ecommerce.entity.Product;
+import com.huskycode.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+        HttpMethod[] theUnsupportedActions = {HttpMethod.DELETE,HttpMethod.PUT,HttpMethod.POST};
+
+        //disable http methods for put,post and delete
+        config.getExposureConfiguration().
+                forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        config.getExposureConfiguration().
+                forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
+}
