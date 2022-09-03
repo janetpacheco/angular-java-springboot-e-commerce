fa53789506f1c8b82e31ac07ecd70155fe12ba38
janetpacheco <99910759+AnaPachecoMind@users.noreply.github.com>
2022-09-03 15:47:37 -0600

    s1 define routes 76

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/app.module.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -5,6 +5,18 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http'
 import { ProductService } from './services/product.service';
+import {Routes, RouterModule} from '@angular/router'
+
+// 1. Define your routes going from the most specific to the most generic
+const routes: Routes =[
+  {path: 'category/:id',component: ProductListComponent},
+  {path: 'category',component: ProductListComponent},
+  {path: 'products',component: ProductListComponent},
+  {path: '', redirectTo:'/products', pathMatch:'full'},
+  {path: '**', redirectTo:'/products', pathMatch:'full'}
+
+
+];
 @NgModule({
   declarations: [
     AppComponent,
