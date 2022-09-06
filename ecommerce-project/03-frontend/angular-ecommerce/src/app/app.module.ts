0663f0a723d54b691700fec2c624ee159780bd79
janetpacheco <anne.janeta@gmail.com>
2022-09-06 14:06:38 -0600

    s3 Create new component for menu 84

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/app.module.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http'
 import { ProductService } from './services/product.service';
-import {Routes, RouterModule} from '@angular/router'
+import {Routes, RouterModule} from '@angular/router';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component'
 
 // 1. Define your routes going from the most specific to the most generic
 const routes: Routes =[
@@ -20,7 +21,8 @@ const routes: Routes =[
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductCategoryMenuComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
