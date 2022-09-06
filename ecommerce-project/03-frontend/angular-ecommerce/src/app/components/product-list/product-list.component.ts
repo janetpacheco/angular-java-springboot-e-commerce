0a9e969ec3b2fba18a6e15a077f383bfb8f16fad
janetpacheco <anne.janeta@gmail.com>
2022-09-06 16:19:19 -0600

    bonus section

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -5,42 +5,45 @@ import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list-grid.component.html',  
+  templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-  products : Product[]= [];
+  products: Product[] = [];
   currentCategoryId: number = 1;
+  currentCategoryName: string = 'Books';
+
   constructor(private productService: ProductService,
-              private route: ActivatedRoute) { }
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(() => {
       this.listProducts();
     });
-   
+
 
   }
 
-  listProducts(){
+  listProducts() {
     // check if "id" parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
-    
-    if (hasCategoryId){
+
+    if (hasCategoryId) {
       //get the "id" param string convert to a number using the "+" symbol 
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
-    }
-    else{
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name')!;
+    } else {
       // not category available : default will be 1
       this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
     }
 
     // get the products for the given category id
     this.productService.getProductList(this.currentCategoryId).subscribe(
-      data=>{
-      this.products = data;
-    }
-  )
+      data => {
+        this.products = data;
+      }
+    )
   }
 
 }
