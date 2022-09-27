d92e00fdcb7dfbe036b85180b599fb06df147b3d
janetpacheco <anne.janetjimenez@gmail.com>
2022-09-26 19:38:50 -0600

    162 update add event handler

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -75,6 +75,26 @@ export class CheckoutComponent implements OnInit {
 
   }
 
+  handleMonthsAndYears(){
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCardInfo');
+    const currentYear : number = new Date().getFullYear();
+    const selectedYear : number = Number(creditCardFormGroup.value.expYear);
+
+    let startMonth: number;
+
+    if ( currentYear === selectedYear){
+      startMonth = new Date().getMonth() + 1 ;
+    }else{
+      startMonth = 1;
+    }
+
+    this.huskyShopFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data ;
+      }
+    );
+  }
 
 
   copyShippingAdressToBillingAddress(event){
