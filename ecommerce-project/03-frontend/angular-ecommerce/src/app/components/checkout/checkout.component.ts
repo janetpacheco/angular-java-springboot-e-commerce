3cf8e8e41e01c66db0dbe5e736801fbc6219de51
janetpacheco <anne.janeta@gmail.com>
2022-09-27 20:19:01 -0600

    apply the rule in html

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
 import { HuskyShopFormService } from 'src/app/services/husky-shop-form-service.service';
+import { HuskyShopValidators } from 'src/app/validators/husky-shop-validators';
 
 @Component({
   selector: 'app-checkout',
@@ -33,8 +34,14 @@ export class CheckoutComponent implements OnInit {
     this.checkoutFormGroup = this.formBuilder.group(
       {
         customer : this.formBuilder.group({
-          firstName: new FormControl('',[Validators.required,Validators.minLength(2)]),
-          lastName: new FormControl('',[Validators.required,Validators.minLength(2)]),
+          firstName: new FormControl('',
+                                       [Validators.required,
+                                        Validators.minLength(2), 
+                                        HuskyShopValidators.notOnlyWhitespace]),
+          lastName: new FormControl('',[Validators.required,
+                                        Validators.minLength(2),
+                                        HuskyShopValidators.notOnlyWhitespace]),
+                                                            
           email: new FormControl('',
                                 [Validators.required,
                                  Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')])
--- ecommerce-project/03-frontend/angular-ecommerce/src/app/validators/husky-shop-validators.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/validators/husky-shop-validators.ts
@@ -1,15 +1,21 @@
 import { FormControl, ValidationErrors } from "@angular/forms";
 
 export class HuskyShopValidators {
-    static notOnlyWhitespace(control: FormControl): ValidationErrors{
-        if((control.value != null) && (control.value.trim().length===0)){
-            // invalid
-            return{'notOnlyWhitespace': true}
-        }else{
-            // valid
+    
+    // whitespace validation
+    static notOnlyWhitespace(control: FormControl) : ValidationErrors {
+        
+        // check if string only contains whitespace
+        if ((control.value != null) && (control.value.trim().length === 0)) {
+ 
+            // invalid, return error object
+            return { 'notOnlyWhitespace': true };
+        }
+        else {
+            // valid, return null
             return null;
-
         }
     }
-
 }
+ 
+
