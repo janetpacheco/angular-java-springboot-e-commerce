b803cce2e29e9a333e9f1da00c66a354c2dd42a3
janetpacheco <anne.janeta@gmail.com>
2022-10-15 16:49:32 -0600

    add annotated detection strategy

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/huskycode/ecommerce/dao/ProductRepository.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/huskycode/ecommerce/dao/ProductRepository.java
@@ -3,12 +3,13 @@ package com.huskycode.ecommerce.dao;
 import com.huskycode.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 
-
+@RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product,Long> {
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
     Page<Product> findByNameContaining(@Param("name") String name,Pageable pageable);
