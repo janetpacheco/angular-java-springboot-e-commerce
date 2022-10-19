fdd2d2fc9603fb0c3c69ed42bee494045df69fad
janetpacheco <anne.janeta@gmail.com>
2022-10-18 23:04:00 -0600

    add support for orders history

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -30,6 +30,8 @@ export class CheckoutComponent implements OnInit {
 
   shippingAddressStates: State[] = [];
   billingAddressStates: State[] = [];
+
+  storage :Storage= sessionStorage;
   
 
   constructor(private formBuilder: FormBuilder,
@@ -42,6 +44,9 @@ export class CheckoutComponent implements OnInit {
 
     this.reviewCartDetails();
 
+    //read from browser storage
+    const theEmail = JSON.parse(this.storage.getItem('userEmail')!);
+
     this.checkoutFormGroup = this.formBuilder.group(
       {
         customer : this.formBuilder.group({
@@ -53,7 +58,7 @@ export class CheckoutComponent implements OnInit {
                                         Validators.minLength(2),
                                         HuskyShopValidators.notOnlyWhitespace]),
                                                             
-          email: new FormControl('',
+          email: new FormControl(theEmail,
                                 [Validators.required,
                                  Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')])
         }),
