6be2893b1f0448650f36560bc5b7e8bf0d3adc81
janetpacheco <anne.janeta@gmail.com>
2022-10-19 17:16:29 -0600

    update redirectUri https

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
     oidc :{
         clientId:'0oa6uphv6mksgs9AA5d7',
         issuer:'https://dev-29720397.okta.com/oauth2/default',
-        redirectUri:'http://localhost:4200/login/callback',
+        redirectUri:'https://localhost:4200/login/callback',
         scopes : ['openid','profile','email']
     }
 }
