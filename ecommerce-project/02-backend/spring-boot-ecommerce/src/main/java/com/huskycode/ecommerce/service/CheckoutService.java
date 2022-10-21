b18f4d62778b03efc4cb99888207acf1f09881d6
janetpacheco <99910759+AnaPachecoMind@users.noreply.github.com>
2022-10-21 13:18:17 -0600

    create paymentIntent update CheckoutServ

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/huskycode/ecommerce/service/CheckoutService.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/huskycode/ecommerce/service/CheckoutService.java
@@ -1,8 +1,12 @@
 package com.huskycode.ecommerce.service;
 
+import com.huskycode.ecommerce.dto.PaymentInfo;
 import com.huskycode.ecommerce.dto.Purchase;
 import com.huskycode.ecommerce.dto.PurchaseResponse;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 public interface CheckoutService {
     PurchaseResponse placeOrder(Purchase purchase);
+    PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException;
 }
