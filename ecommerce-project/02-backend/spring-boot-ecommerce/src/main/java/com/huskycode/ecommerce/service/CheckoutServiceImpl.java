a4961473f586ea1237dd30869992c7b990650784
janetpacheco <99910759+AnaPachecoMind@users.noreply.github.com>
2022-10-21 13:18:17 -0600

    create paymentIntent update CheckoutServ

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/huskycode/ecommerce/service/CheckoutServiceImpl.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/huskycode/ecommerce/service/CheckoutServiceImpl.java
@@ -1,17 +1,21 @@
 package com.huskycode.ecommerce.service;
 
 import com.huskycode.ecommerce.dao.CustomerRepository;
+import com.huskycode.ecommerce.dto.PaymentInfo;
 import com.huskycode.ecommerce.dto.Purchase;
 import com.huskycode.ecommerce.dto.PurchaseResponse;
 import com.huskycode.ecommerce.entity.Customer;
 import com.huskycode.ecommerce.entity.Order;
 import com.huskycode.ecommerce.entity.OrderItem;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService{
@@ -19,8 +23,12 @@ public class CheckoutServiceImpl implements CheckoutService{
     private CustomerRepository customerRepository;
 
     @Autowired
-    public CheckoutServiceImpl(CustomerRepository customerRepository){
+    public CheckoutServiceImpl(CustomerRepository customerRepository,
+                               @Value("${stripe.key-secret}") String secretKey){
+
         this.customerRepository = customerRepository;
+        // start Stripe API with secret key
+        Stripe.apiKey = secretKey;
     }
     @Override
     @Transactional
@@ -52,6 +60,19 @@ public class CheckoutServiceImpl implements CheckoutService{
         customerRepository.save(customer);
         return new PurchaseResponse(orderTrackingNumber) ;
     }
+
+    @Override
+    public PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException {
+        List<String> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", paymentInfo.getAmount());
+        params.put("currrency",paymentInfo.getCurrency());
+        params.put("payment_method_types", paymentMethodTypes);
+        return PaymentIntent.create(params);
+    }
+
 
     private String generateOrderTrackingNumber() {
         //UUID
