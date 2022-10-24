036a86b95b3a0d2d17eb9d3fa7cf82bff486f405
janetpacheco <anne.janeta@gmail.com>
2022-10-24 15:04:11 -0600

    Add description

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/huskycode/ecommerce/service/CheckoutServiceImpl.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/huskycode/ecommerce/service/CheckoutServiceImpl.java
@@ -75,6 +75,8 @@ public class CheckoutServiceImpl implements CheckoutService{
         params.put("amount", paymentInfo.getAmount());
         params.put("currency",paymentInfo.getCurrency());
         params.put("payment_method_types", paymentMethodTypes);
+        params.put("description", "HuskyCode purchase");
+
         return PaymentIntent.create(params);
     }
 
