ea942a29919e0211188842e89aba4b05b601e99b
janetpacheco <anne.janeta@gmail.com>
2022-10-24 16:47:31 -0600

    move supporting files

--- typescript-training/01-helloworld/helloworld.js
+++
@@ -1,5 +0,0 @@
-console.log('Hello world');
-console.log('Happy birthday Annie' + ' atte Adelita!');
-var firstName = 'Ana';
-var age = 29;
-console.log('Hi ' + firstName + ' today is your birthday ' + age);
